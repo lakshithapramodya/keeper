59ded5020da74e5bddf9ae5501fc867d56e030f0
Lakshitha <pramodya8@gmail.com>
2023-04-27 22:19:09 +0530

    new project

---
+++ .history/src/components/Note_20230427221434.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { TrashIcon } from "@heroicons/react/24/solid";
+
+function Note(props) {
+  function handleClick() {
+    props.onDelete(props.id);
+  }
+
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button className="note button" onClick={handleClick}>
+        <TrashIcon />
+      </button>
+    </div>
+  );
+}
+
+export default Note;
